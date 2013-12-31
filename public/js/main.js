cf16610f925ca909108952ca6e670f1305c470f7
Swader <bruno@skvorc.me>
2013-12-31 01:01:10 +0100

    Minor change for demo

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
    $("#checkdateButton").click(function() {
-       $.get('/check.php', {day: $('#day').val(), month: $('#month').val(), year: $("#year").val()}, function(data) {
+       $.get('check.php', {day: $('#day').val(), month: $('#month').val(), year: $("#year").val()}, function(data) {
            $("#output").text(data.message);
        }, "json");
    });
